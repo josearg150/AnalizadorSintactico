a4bef2a3da3d7abad4cbf1f92b23ebc6bd08ba6a
jdanielh24 <jdanielopez24@gmail.com>
2021-06-29 15:48:20 -0500

    Cambiar diseño
    Cambiar colores y fuentes al diseño

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -34,64 +34,66 @@ void MainWindow::on_actionAbrir_triggered()
 {
     QString archivoAbierto = QFileDialog::getOpenFileName(this, tr("Abrir Archivo"), ".", tr("Archivos (*.dat)"));
     std::string archivoAbiertoStr = archivoAbierto.toLocal8Bit().constData();
-    ui->tokens->clearContents();
-    ui->producciones->clearContents();
-    ui->tokens->setRowCount(0);
-    ui->producciones->setRowCount(0);
+
 
     switch (analisisLex(archivoAbiertoStr)) {
-        case 0: {
-            ui->textBrowser->clear();
-            std::ifstream t(archivoAbiertoStr);
-            std::stringstream buffer;
-            buffer << t.rdbuf();
-            QString qstr = QString::fromStdString(buffer.str());
-            ui->textBrowser->append(qstr);
-            for (int i = 0; i < returnK(); i++) {
-                ui->tokens->insertRow(i);
-                QTableWidgetItem *aux = new QTableWidgetItem(returnAsTokens()[i]);
-                aux->setTextAlignment(Qt::AlignCenter);
-                ui->tokens->setItem(i, 0, aux);
+    case 0: {
+        ui->tokens->clearContents();
+        ui->producciones->clearContents();
+        ui->tokens->setRowCount(0);
+        ui->producciones->setRowCount(0);
+        ui->textBrowser->clear();
+        std::ifstream t(archivoAbiertoStr);
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+        QString qstr = QString::fromStdString(buffer.str());
+        ui->textBrowser->append(qstr);
+        for (int i = 0; i < returnK(); i++) {
+            ui->tokens->insertRow(i);
+            QTableWidgetItem *aux = new QTableWidgetItem(returnAsTokens()[i]);
+            aux->setTextAlignment(Qt::AlignCenter);
+            ui->tokens->setItem(i, 0, aux);
+        }
+        if (analisisSin() == 0) {
+            for (int j = 0; j < 50; j++) {
+                std::string str = as->producciones[j];
+                if (str == "") {
+                    break;
+                }
+                QString qstr = QString::fromStdString(str);
+                ui->producciones->insertRow(j);
+                QTableWidgetItem *aux = new QTableWidgetItem(qstr);
+                aux->setTextAlignment(Qt::AlignLeft);
+                ui->producciones->setItem(j, 0, aux);
             }
-            break;
-        }
-        case 1: {
+        } else {
             QMessageBox* msgbox = new QMessageBox(this);
             msgbox->setAttribute(Qt::WA_DeleteOnClose);
             msgbox->setWindowTitle("Aviso");
-            msgbox->setText("No se seleccionó ningún archivo.");
+            msgbox->setText("Error de sintaxis.");
             msgbox->open();
-            break;
         }
-        case 2: {
-            QMessageBox* msgbox = new QMessageBox(this);
-            msgbox->setAttribute(Qt::WA_DeleteOnClose);
-            msgbox->setWindowTitle("Aviso");
-            msgbox->setText("El archivo está vacío.");
-            msgbox->open();
-            break;
-        }
+        break;
     }
-
-    if (analisisSin() == 0) {
-        for (int j = 0; j < 50; j++) {
-            std::string str = as->producciones[j];
-            if (str == "") {
-                break;
-            }
-            QString qstr = QString::fromStdString(str);
-            ui->producciones->insertRow(j);
-            QTableWidgetItem *aux = new QTableWidgetItem(qstr);
-            aux->setTextAlignment(Qt::AlignLeft);
-            ui->producciones->setItem(j, 0, aux);
-        }
-    } else {
+    case 1: {
         QMessageBox* msgbox = new QMessageBox(this);
         msgbox->setAttribute(Qt::WA_DeleteOnClose);
         msgbox->setWindowTitle("Aviso");
-        msgbox->setText("Error de sintaxis.");
+        msgbox->setText("No se seleccionó ningún archivo.");
         msgbox->open();
+        break;
     }
+    case 2: {
+        QMessageBox* msgbox = new QMessageBox(this);
+        msgbox->setAttribute(Qt::WA_DeleteOnClose);
+        msgbox->setWindowTitle("Aviso");
+        msgbox->setText("El archivo está vacío.");
+        msgbox->open();
+        break;
+    }
+    }
+
+
 
     /*
     switch (analisisSin()) {
